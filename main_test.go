d85dde497f80f0f47fe111a2c5a4d11d36162958
Jef Spaleta <jspaleta@gmail.com>
2021-04-16 15:56:04 -0800

    add doQuery test that processes to false

--- main_test.go
+++ main_test.go
@@ -10,47 +10,60 @@ func TestMain(t *testing.T) {
 
 func TestQuery(t *testing.T) {
 	tests := []struct {
-		name           string
-		url            string
-		request_type   string
-		request_data   string
-		jscript        string
-		expect_error   bool
-		expected_value bool
+		name                   string
+		url                    string
+		request_type           string
+		request_data           string
+		jscript                string
+		expect_query_error     bool
+		expect_process_error   bool
+		expected_process_value bool
 	}{
 		{
-			name:           `GET "http://httpbin.org/delay/1"`,
-			url:            `http://httpbin.org/delay/1`,
-			request_type:   `GET`,
-			request_data:   "",
-			expect_error:   false,
-			expected_value: false,
-			jscript:        ``,
+			name:                   `GET "http://httpbin.org/delay/1"`,
+			url:                    `http://httpbin.org/delay/1`,
+			request_type:           `GET`,
+			request_data:           "",
+			expect_query_error:     false,
+			expect_process_error:   false,
+			expected_process_value: false,
+			jscript:                ``,
 		},
 		{
-			name:           `POST "https://httpbin.org/post"`,
-			url:            `http://httpbin.org/post`,
-			request_type:   `POST`,
-			request_data:   `{"test":"value"}`,
-			expect_error:   false,
-			expected_value: true,
-			jscript:        `data.json.test === "value"`,
+			name:                   `POST "https://httpbin.org/post"`,
+			url:                    `http://httpbin.org/post`,
+			request_type:           `POST`,
+			request_data:           `{"test":"value"}`,
+			expect_query_error:     false,
+			expect_process_error:   false,
+			expected_process_value: true,
+			jscript:                `data.json.test === "value"`,
+		},
+		{
+			name:                   `POST "https://httpbin.org/post"`,
+			url:                    `http://httpbin.org/post`,
+			request_type:           `POST`,
+			request_data:           `{"test":"value"}`,
+			expect_query_error:     false,
+			expect_process_error:   false,
+			expected_process_value: false,
+			jscript:                `data.json.test === "bad value"`,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			result, err := doQuery(tt.url, tt.request_type, strings.NewReader(tt.request_data))
-			if !tt.expect_error && err != nil {
+			if !tt.expect_query_error && err != nil {
 				t.Errorf("doQuery() url: %v, body: %v err: %v\n", tt.url, string(result), err)
 				return
 			}
 			presult, err := processResponse(string(result), tt.jscript)
-			if !tt.expect_error && err != nil {
+			if !tt.expect_process_error && err != nil {
 				t.Errorf("processResponse() json_data: %v, jscript: %v, err: %v\n", string(result), tt.jscript, err)
 				t.Errorf("doQuery() url: %v, body: %v err: %v\n", tt.url, string(result), err)
 				return
 			}
-			if presult != tt.expected_value {
+			if presult != tt.expected_process_value {
 				t.Errorf("processResponse() json_data: %v, jscript: %v, err: %v\n", string(result), tt.jscript, err)
 				t.Errorf("doQuery() url: %v, body: %v err: %v\n", tt.url, string(result), err)
 				return
